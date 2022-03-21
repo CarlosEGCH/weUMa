86442559a281940ba194beb07029804dae54bedf
CarlosEGCH <carlitosgch4@gmail.com>
2022-03-21 21:00:46 +0000

    Responsive navbar implemented without links

--- weuma/frontend/src/components/Drawer.js
+++ weuma/frontend/src/components/Drawer.js
@@ -1,7 +1,18 @@
 import * as React from 'react';
-import {Button, Drawer, DrawerOverlay, DrawerCloseButton, DrawerBody, Input,
+import { Box, Text, Spacer, Flex, Center, Grid, GridItem, Button, Drawer, DrawerOverlay, DrawerCloseButton, DrawerBody, Input,
      DrawerHeader, DrawerFooter, DrawerContent, useDisclosure, Image} from '@chakra-ui/react';
 import menuIcon from '../assets/hamburger-icon.svg';
+
+import mainIcon from '../assets/main-icon.svg';
+import ticketIcon from '../assets/ticket-icon.svg';
+import forumIcon from '../assets/forum-icon.svg';
+import peopleIcon from '../assets/people-icon.svg';
+
+import profileIcon from '../assets/user-icon.svg';
+import storageIcon from '../assets/storage-icon.svg';
+import inboxIcon from '../assets/inbox-icon.svg';
+
+import userImage from '../assets/user.jpg';
 
 export default function DrawerExample() {
   const { isOpen, onOpen, onClose } = useDisclosure()
@@ -19,19 +30,62 @@ export default function DrawerExample() {
         finalFocusRef={btnRef}
       >
         <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader>Create your account</DrawerHeader>
+        <DrawerContent bg='brand.secondary'>
+          <DrawerCloseButton color='brand.accent' />
+          <DrawerHeader color='brand.accent'>
+            <Center>
+                  <Image boxSize='60px' borderRadius='full' src={userImage}></Image>
+                  <Text color='brand.accent' ml='10px' fontSize='20px'>Carlos Gomes</Text>
+                </Center>
+          </DrawerHeader>
 
           <DrawerBody>
-            <Input placeholder='Type here...' />
+            <Grid templateRows='repeat(7, 1fr)' templateColumns='1fr' h='100%'>
+              <GridItem rowStart='1'>
+                <Center h='100%'>
+                  <Text fontSize='35px' color='brand.accent'>
+                    Where do you wanna go?
+                  </Text>
+                </Center>
+              </GridItem>
+              <GridItem rowSpan={2}>
+                <Center h='100%'>
+                  <Image boxSize='60px' src={profileIcon}></Image>
+                  <Image boxSize='60px' ml='20px' mr='20px' src={storageIcon}></Image>
+                  <Image boxSize='60px' src={inboxIcon}></Image>
+                </Center>
+              </GridItem>
+              <GridItem rowStart='4'>
+                <Center>
+                      <Image boxSize='60px' src={mainIcon} mr='20px'></Image>
+                      <Text fontSize='30px' color='brand.accent'>FAQ</Text>
+                </Center>
+              </GridItem>
+              <GridItem rowStart='5'>
+                <Center>
+                      <Image boxSize='60px' src={ticketIcon} mr='20px'></Image>
+                      <Text fontSize='30px' color='brand.accent'>Forum</Text>
+                </Center>
+              </GridItem>
+              <GridItem rowStart='6'>
+                <Center>
+                      <Image boxSize='60px' src={forumIcon} mr='20px'></Image>
+                      <Text fontSize='30px' color='brand.accent'>People</Text>
+                </Center>
+              </GridItem>
+              <GridItem rowStart='7'>
+                <Center>
+                      <Image boxSize='60px' src={peopleIcon} mr='20px'></Image>
+                      <Text fontSize='30px' color='brand.accent'>Tickets</Text>
+                </Center>
+              </GridItem>
+            </Grid>
           </DrawerBody>
 
           <DrawerFooter>
-            <Button variant='outline' mr={3} onClick={onClose}>
-              Cancel
+            <Button variant='outline' bg='brand.accent' mr={3} onClick={onClose}>
+              Logout
             </Button>
-            <Button colorScheme='blue'>Save</Button>
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
